b9062cef03f55a4d75fc9ff24ab972b29a7346da
dypick <catracoon@yandex.ru>
2019-06-24 09:10:50 +0300

    Added raw json file

--- app/src/main/java/com/example/devgovappeal/MapActivity.java
+++ app/src/main/java/com/example/devgovappeal/MapActivity.java
@@ -1,37 +1,57 @@
 package com.example.devgovappeal;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.app.Activity;
 
 import com.yandex.mapkit.Animation;
 import com.yandex.mapkit.MapKitFactory;
+import com.yandex.mapkit.geometry.Circle;
+import com.yandex.mapkit.geometry.LinearRing;
 import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.geometry.Polygon;
 import com.yandex.mapkit.map.CameraPosition;
 
+import com.yandex.mapkit.map.CircleMapObject;
+import com.yandex.mapkit.map.CompositeIcon;
+import com.yandex.mapkit.map.MapObject;
+import com.yandex.mapkit.map.MapObjectCollection;
+import com.yandex.mapkit.map.PlacemarkMapObject;
+import com.yandex.mapkit.map.PolygonMapObject;
 import com.yandex.mapkit.mapview.MapView;
+import com.yandex.runtime.image.AnimatedImageProvider;
+import com.yandex.runtime.image.ImageProvider;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class MapActivity extends Activity {
-    private final Point TARGET_LOCATION = new Point(52.971381, 36.063083); // Долгота и широта, соответствующие зданию администрации области
+    private final Point DRAGGABLE_PLACEMARK_CENTER = new Point(59.948, 30.323);
+    private final Point TARGET_LOCATION = new Point(52.971381, 36.063083);
 
-    private MapView mapView; // экземпляр, необходимый для привязки к элементу MapView на layout приложения
+    private MapView mapView;
+    private MapObjectCollection mapObjects;
 
     @Override
-    // действие, выполняемое после генерации layout на экране девайса.
-    // Хавает ключ, выполняет привязку к элементу mapview на экране
-    // Плавно перемещается на заданные коор-ты
     protected void onCreate(Bundle savedInstanceState) {
-        // строка с Ключом Разработчка Yandex. ключ доступен по адресу : https://developer.tech.yandex.ru/ website
         String MAPKIT_API_KEY = "44655ba5-c7a0-4cf4-82d4-2116fd9485e0";
         MapKitFactory.setApiKey(MAPKIT_API_KEY);
         MapKitFactory.initialize(this);
         setContentView(R.layout.activity_map_layout);
         super.onCreate(savedInstanceState);
         mapView = findViewById(R.id.mapview);
-
+        mapObjects = mapView.getMap().getMapObjects().addCollection();
         mapView.getMap().move(
                 new CameraPosition(TARGET_LOCATION, 14.0f, 0.0f, 0.0f),
                 new Animation(Animation.Type.SMOOTH, 5),
                 null);
+
+        MapObjectCollection mapObjectCollection = mapView.getMap().getMapObjects().addCollection();
     }
 
     @Override
